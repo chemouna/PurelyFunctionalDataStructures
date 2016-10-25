178be971fec7585288300500071bb8c5f02f192e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-10-25 09:31:54 +0200

    Add batched queue.

---
+++ okasaki-haskell/src/Ch05/Queue.hs
@@ -0,0 +1,40 @@
+module Ch05 where
+
+import Prelude hiding (head, tail)
+
+class Queue q where
+  empty :: q a
+  isEmpty :: q a -> Bool
+
+  snoc :: q a -> a -> q a
+  head :: q a -> a
+  tail :: q a -> q a
+
+fromList :: Queue q => [a] -> q a
+fromList = foldl snoc empty
+
+toList :: Queue q  => q a -> [a]
+toList q | isEmpty q = []
+         | otherwise = head q : toList (tail q)
+
+
+-- Queue
+data BatchedQueue a = BQ [a] [a]
+  deriving (Eq, Show)
+
+checkf [] r = BQ (reverse r) []
+checkf f r = BQ f r
+
+instance Queue BatchedQueue where
+  empty = BQ [] []
+  isEmpty (BQ f r) = null f --because we have the invariant if front is empty, rear must be too
+  snoc (BQ f r) x = checkf f (x:r) 
+
+  head (BQ [] _) = error "BatchedQueue.head : the queue is empty"
+  head (BQ (x:f) r) = x
+
+  tail (BQ [] _) =  error "BatchedQueue.head : the queue is empty"
+  tail (BQ (x:f) r) = checkf f r
+
+
+-- Queue Tests
