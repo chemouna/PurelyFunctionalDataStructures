1210e631fa2846cff9dcf80433a6350efe48d73d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-10-26 00:02:22 +0200

    Adding some quickcheck properties tests.

--- okasaki-haskell/src/Ch05/Queue.hs
+++ okasaki-haskell/src/Ch05/Queue.hs
@@ -1,24 +1,25 @@
 module Ch05 where
 
-import Prelude hiding (head, tail)
+import Prelude hiding (head, tail, length)
 import Test.QuickCheck
 import Test.QuickCheck.All
+import qualified Data.List as List
 
 class Queue q where
   empty :: q a
   isEmpty :: q a -> Bool
 
   snoc :: q a -> a -> q a
-  head :: q a -> a
-  tail :: q a -> q a
+  headq :: q a -> a
+  tailq :: q a -> q a
+  length :: q a -> Int
 
 fromList :: Queue q => [a] -> q a
 fromList = foldl snoc empty
 
 toList :: Queue q  => q a -> [a]
 toList q | isEmpty q = []
-         | otherwise = head q : toList (tail q)
-
+         | otherwise = headq q : toList (tailq q)
 
 -- Queue
 data BatchedQueue a = BQ [a] [a]
@@ -30,17 +31,24 @@ checkf f r = BQ f r
 instance Queue BatchedQueue where
   empty = BQ [] []
   isEmpty (BQ f r) = null f --because we have the invariant if front is empty, rear must be too
-  snoc (BQ f r) x = checkf f (x:r) 
+  snoc (BQ f r) x = checkf f (x:r)
 
-  head (BQ [] _) = error "BatchedQueue.head : the queue is empty"
-  head (BQ (x:f) r) = x
+  headq (BQ [] _) = error "BatchedQueue.head : the queue is empty"
+  headq (BQ (x:f) r) = x
 
-  tail (BQ [] _) =  error "BatchedQueue.head : the queue is empty"
-  tail (BQ (x:f) r) = checkf f r
-
+  tailq (BQ [] _) =  error "BatchedQueue.head : the queue is empty"
+  tailq (BQ (x:f) r) = checkf f r
+  length (BQ [] _) = 0
+  length (BQ (x:f) []) = 1 + List.length f
+  length (BQ (x:f) (y:r)) = 1 + List.length f + 1 + List.length r
 
 -- Queue Tests
 -- applying toList fromList gives back the same list
 prop_toListFromListIdempotent xs = toList (fromList' xs) == xs
   where fromList' :: [a] -> BatchedQueue a
         fromList' = fromList
+
+-- | Validates that the length of a queue is the same as the length of the
+--   list generated from the queue.
+prop_length_toList :: BatchedQueue q => q -> Bool
+prop_length_toList q = List.length (toList q) == length q
